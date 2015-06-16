811bf0c75a4dbae518ebba8a730ebab9773cd88c
Manfred Touron <m@42.am>
2015-06-16 18:59:45 +0200

    Split scripts for multi-arch (#53)

---
+++ rules-Openbsd-armv7.mk
@@ -0,0 +1,18 @@
+.PHONY: uInitrd
+uInitrd:
+	@echo "not implemented"
+
+
+.PHONY: travis
+travis:
+	@echo "not implemented"
+
+
+.PHONY: publish_on_s3
+publish_on_s3:
+	@echo "not implemented"
+
+
+.PHONY: dist
+dist:
+	@echo "not implemented"
